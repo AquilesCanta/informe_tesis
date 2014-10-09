7bd0f8cde926dda82aed6aee420264a4fd7f72d1
sseifert <santiagoseifert@gmail.com>
2014-10-09 16:31:35 -0300

    Modified script of compilation. Added contents to technical introduction

--- gogopdf.sh
+++ gogopdf.sh
@@ -1,1 +1,4 @@
-pdflatex -c-style-errors -output-directory=output -aux-directory=aux tesis
+if [ "$#" -gt "0" ]
+then echo "CAP"$1; pdflatex -c-style-errors -output-directory=output -aux-directory=aux $1; fi
+if [ "$#" -eq "0" ]
+then echo "TESIS"; pdflatex -c-style-errors -output-directory=output -aux-directory=aux tesis; fi
