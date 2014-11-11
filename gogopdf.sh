672a52f77a671f3b97597ef248c4c91b473b819f
AquilesCanta <santiagoseifert@gmail.com>
2014-11-10 23:32:57 -0300

    Added bibliography. Further improvement of technical intro

--- gogopdf.sh
+++ gogopdf.sh
@@ -1,4 +1,4 @@
 if [ "$#" -gt "0" ]
-then echo "CAP"$1; pdflatex -output-directory=output -aux-directory=aux $1; fi
+then echo "CAP"$1; pdflatex -output-directory=output $1; fi
 if [ "$#" -eq "0" ]
-then echo "TESIS"; pdflatex -output-directory=output -aux-directory=aux tesis; fi
+then echo "TESIS"; pdflatex -output-directory=output tesis; fi
