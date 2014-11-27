7b15f3eaa7f8e9e8d590a53192ebe7f1212f0c4c
sseifert <santiagoseifert@gmail.com>
2014-11-27 16:12:05 -0300

    Revised version of technical intro. Added some code to opencaster intro

---
+++ src_code/pmt_example.py
@@ -0,0 +1,20 @@
+pmt = program_map_section(
+	program_number = 59202,
+	PCR_PID = 2064,
+	program_info_descriptor_loop = [],
+	stream_loop = [
+		stream_loop_item(
+			stream_type = 2,
+			elementary_PID = 2064,
+			element_info_descriptor_loop = []
+		),
+		stream_loop_item(
+			stream_type = 3,
+			elementary_PID = 2068,
+			element_info_descriptor_loop = []
+			),
+		],
+		version_number = 1,
+		section_number = 0,
+		last_section_number = 0,
+)
