f6a21998464cfe8bacf76afa715caf98f40fb2f0
sseifert <santiagoseifert@gmail.com>
2014-12-02 16:30:56 -0300

    Design from lab to home. Added an image

---
+++ src_code/pat_gen.py
@@ -0,0 +1,17 @@
+print "Hello world!"
+
+i = raw_input("Dame algo")
+
+print i + 1
+
+for i in range(1,10):
+	print "Anda a cagar"
+
+print "Hello world!"
+
+i = raw_input("Dame algo")
+
+print i + 1
+
+for i in range(1,10):
+	print "Anda a cagar"
---
+++ src_code/sdt_gen.py
@@ -0,0 +1,8 @@
+print "Hello world!"
+
+i = raw_input("Dame algo")
+
+print i + 1
+
+for i in range(1,10):
+	print "Anda a cagar"
