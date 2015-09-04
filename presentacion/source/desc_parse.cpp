5f3a10ced539acff52a508a7113c5f7668a62083
Santiago Seifert <santiagoseifert@gmail.com>
2015-09-04 11:48:43 -0300

    Final Presentation version?

--- presentacion/source/desc_parse.cpp
+++ presentacion/source/desc_parse.cpp
@@ -1,22 +1,10 @@
-bool fnc_elementary_streams_relocation(
-	     ElementaryStreamsRelocationDescriptor &desc,
-	     BYTE *descPayload){
-	DTV_ASSERT(DESC_TAG(descPayload)
-		       == ident::elementary_streams_relocation );
 	std::string ip;
-	using boost::lexical_cast;
-	using std::string;
-	ip += lexical_cast<string>(static_cast<int>(descPayload[2]));
+	ip += lexical_cast<string>(descPayload[2]);
 	ip += ".";
-	ip += lexical_cast<string>(static_cast<int>(descPayload[3]));
+	ip += lexical_cast<string>(descPayload[3]);
 	ip += ".";
-	ip += lexical_cast<string>(static_cast<int>(descPayload[4]));
+	ip += lexical_cast<string>(descPayload[4]);
 	ip += ".";
-	ip += lexical_cast<string>(static_cast<int>(descPayload[5]));
+	ip += lexical_cast<string>(descPayload[5]);
 	desc.ipMulticast = ip;
 	desc.port = (descPayload[6] << 8) + descPayload[7];
-	LTRACE("desc",
-		   "Elementary Streams Relocation Descriptor found. IP = %s PORT = %d",
-		   ip.c_str(), desc.port);
-	return true;
-}
--- presentacion/source/pmt_gen.py
+++ presentacion/source/pmt_gen.py
@@ -2,7 +2,8 @@ pmt_universidad = program_map_section(
 	program_number = SERVICE_UNIVERSIDAD_ID,
 	PCR_PID = 8191,
 	program_info_descriptor_loop = [
-		elementary_streams_relocation_descriptor(target_multicast_group="239.1.1.1", target_multicast_port=1000)
+		elementary_streams_relocation_descriptor(target_multicast_group="239.1.1.1",
+		                                         target_multicast_port=1000)
 	],
 	stream_loop = [],
 	version_number = 0,
@@ -14,7 +15,8 @@ pmt_la_plata = program_map_section(
 	program_number = SERVICE_LA_PLATA_ID,
 	PCR_PID = 8191,
 	program_info_descriptor_loop = [
-		elementary_streams_relocation_descriptor(target_multicast_group="239.1.1.2", target_multicast_port=1000)
+		elementary_streams_relocation_descriptor(target_multicast_group="239.1.1.2",
+		                                         target_multicast_port=1000)
 	],
 	stream_loop = [],
 	version_number = 0,
